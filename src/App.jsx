75c003aa41bfb3539685a6aceabb1f2b08d523ec
Noah Joselito <noahjoselito@gmail.com>
2025-12-02 09:15:48 +0300

    ao

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState } from 'react';
+import React from 'react';
 import "./index.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -8,8 +8,6 @@ import Navbar from "./components/Navbar";
 import Portofolio from "./Pages/Portofolio";
 import ContactPage from "./Pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
-// import WelcomeScreen from "./Pages/WelcomeScreen";
-import { AnimatePresence } from 'framer-motion';
 import ErrorBoundary from './components/ErrorBoundary';
 
 // Configurer les futures flags
@@ -20,32 +18,27 @@ const router = {
   }
 };
 
-const LandingPage = ({ showWelcome, setShowWelcome }) => {
+const LandingPage = () => {
   return (
     <>
-
-      {!showWelcome && (
-        <>
-          <Navbar />
-          <AnimatedBackground />
-          <Home />
-          <About />
-          <Portofolio />
-          <ContactPage />
-          <footer>
-            <center>
-              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  Noah
-                </a>
-                . All Rights Reserved.
-              </span>
-            </center>
-          </footer>
-        </>
-      )}
+      <Navbar />
+      <AnimatedBackground />
+      <Home />
+      <About />
+      <Portofolio />
+      <ContactPage />
+      <footer>
+        <center>
+          <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+          <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+            © 2025{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              Noah
+            </a>
+            . All Rights Reserved.
+          </span>
+        </center>
+      </footer>
     </>
   );
 };
@@ -69,13 +62,11 @@ const ProjectPageLayout = () => (
 );
 
 function App() {
-  const [showWelcome, setShowWelcome] = useState(true);
-
   return (
     <ErrorBoundary>
       <BrowserRouter future={router.future}>
         <Routes>
-          <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/project/:id" element={<ProjectPageLayout />} />
         </Routes>
       </BrowserRouter>
